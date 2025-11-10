1124d48b704b961682f024c5b7e0a3120b8b8771
Noah Hill <noahhill576@aol.com>
2025-11-10 22:33:06 +0700

    add storage link

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Run storage link - for production deployment
+Route::get('/run-storage-link', function () {
+    Artisan::call('storage:link');
+    return response()->json(['message' => 'Storage linked successfully!'], 200);
 });
