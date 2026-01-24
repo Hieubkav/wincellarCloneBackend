0c3a3cfefc295d7ff6ac9068fdedac50c77ed9ff
Noah Hill <noahhill576@aol.com>
2026-01-24 22:29:20 +0700

    feat: Add footer_config to public settings API response

--- app/Http/Resources/V1/SettingResource.php
+++ app/Http/Resources/V1/SettingResource.php
@@ -22,6 +22,7 @@ class SettingResource extends JsonResource
             'hours' => $this->hours,
             'email' => $this->email,
             'google_map_embed' => $this->google_map_embed,
+            'footer_config' => $this->footer_config,
             
             // Logo and favicon URLs
             'logo_url' => $this->logoImage?->url ?? '/placeholder/logo.svg',
